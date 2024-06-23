9c52eee2f1c500edd50621309f069fb29eff38a8
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2024-06-23 09:26:47 +0700

    add modul setting

---
+++ resources/views/setting/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title', 'Setting')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <x-card>
+                <x-slot name="header">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link @if (request('pills') == '') active @endif" href="#settings"
+                                data-toggle="tab">Setting</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link @if (request('pills') == 'logo') active @endif" href="#logo"
+                                data-toggle="tab">Logo</a>
+                        </li>
+                    </ul>
+                </x-slot>
+
+                <div class="tab-content">
+                    <div class="tab-pane @if (request('pills') == '') show active @endif" id="settings">
+                        @include('setting.general')
+                    </div>
+
+                    <div class="tab-pane @if (request('pills') == 'logo') show active @endif" id="logo">
+                        @include('setting.logo')
+                    </div>
+                </div>
+            </x-card>
+        </div>
+    </div>
+@endsection
+
+<x-toast />
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\SensorDataController;
+use App\Http\Controllers\SettingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,10 +12,18 @@ Route::get('/', function () {
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // ROUTE ALAT
     Route::get('/devices/data', [DeviceController::class, 'data'])->name('devices.data');
     Route::resource('/devices', DeviceController::class);
 
+    // ROUTE HISTORY ALAT
     Route::get('/sensordata/data', [SensorDataController::class, 'data'])->name('sensordata.data');
     Route::resource('/sensordata', SensorDataController::class);
     Route::delete('/sensordata/delete_all', [SensorDataController::class, 'destroy'])->name('sensordata.delete_all');
+
+    // ROUTE SETTING
+    Route::get('setting', [SettingController::class, 'index'])->name('setting.index');
+    Route::put('/setting/{setting}', [SettingController::class, 'update'])
+        ->name('setting.update');
 });
