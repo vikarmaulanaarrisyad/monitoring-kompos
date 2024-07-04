9f607803df8e551fd2046ee0eca657770b35c60c
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2024-07-04 19:14:11 +0700

    fix bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Setting;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,11 +25,21 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('*', function ($view) {
             $view->with('setting', Setting::first());
         });
-        view()->composer('*', function ($view) {
-            $view->with('notifications', Auth::user()->notifications()->whereNull('read_at')->where('created_at','DESC')->limit(5)->get());
+        View::composer('*', function ($view) {
+            if (Auth::check()) {
+                $user = Auth::user();
+                $notifications = $user->notifications()->whereNull('read_at')->orderBy('created_at', 'DESC')->limit(5)->get();
+                $notificationCount = $user->notifications()->whereNull('read_at')->count();
+
+                $view->with('notifications', $notifications);
+                $view->with('notificationCount', $notificationCount);
+            }
         });
-        view()->composer('*', function ($view) {
-            $view->with('notificationCount', Auth::user()->notifications()->whereNull('read_at')->where('created_at','DESC')->limit(5)->count());
-        });
+        // view()->composer('*', function ($view) {
+        //     $view->with('notifications', Auth::user()->notifications()->whereNull('read_at')->where('created_at','DESC')->limit(5)->get());
+        // });
+        // view()->composer('*', function ($view) {
+        //     $view->with('notificationCount', Auth::user()->notifications()->whereNull('read_at')->where('created_at','DESC')->limit(5)->count());
+        // });
     }
 }
